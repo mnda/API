2d89c118bfefff8d8c3f65a010c12137df5612c9
Mark Nicholas Angeles <mnangeles@gmail.com>
2017-04-21 17:00:35 -0400

    Master0.1

--- src/test/java/TestLogin.java
+++ src/test/java/TestLogin.java
@@ -21,48 +21,48 @@ public class TestLogin {
     }
 
 
-//    @Test
-//    public void testFBLoginSuccessful() {
-//        //cannot find valid fb auth code
-//    }
-//
-//    @Test
-//    public void testFBLoginInvalid() {
-//        APITesting apitesting = new APITesting();
-//        String responseBody = apitesting.airBNBLogin(TestData.Login.FB_INVALID_LOGIN_PARAMS);
-//        System.out.println("Validate response for invalid login to AirBNB API using invalid fb authorization key");
-//        Assert.assertEquals("Expected and Actual Response does not match",responseBody, TestData.TestLoginExpectedResults.COULD_NOT_AUTHENTICATE_RESPONSE);
-//    }
-//
-//
-//    @Test
-//    public void testEmailLoginSuccessful() {
-//        //cannot find valid email and password
-//    }
-//
-//    @Test
-//    public void testEmailLoginInvalidPassword() {
-//        APITesting apitesting = new APITesting();
-//        String responseBody = apitesting.airBNBLogin(TestData.Login.EMAIL_INVALID_PASSWORD_LOGIN_PARAMS);
-//        System.out.println("Validate response for  invalid login to AirBNB API using  invalid email and valid password");
-//        Assert.assertEquals("Expected and Actual Response does not match",responseBody, TestData.TestLoginExpectedResults.INVALID_CREDENTIALS_RESPONSE);
-//    }
-//
-//    @Test
-//    public void testEmailLoginInvalidEmail() {
-//        APITesting apitesting = new APITesting();
-//        String responseBody = apitesting.airBNBLogin(TestData.Login.EMAIL_INVALID_EMAIL_LOGIN_PARAMS);
-//        System.out.println("Validate response for  invalid login to AirBNB API using valid email and invalid password");
-//        Assert.assertEquals("Expected and Actual Response does not match",responseBody, TestData.TestLoginExpectedResults.INVALID_CREDENTIALS_RESPONSE);
-//    }
-//
-//    @Test
-//    public void testSuspendedEmailLoginInvalid() {
-//        APITesting apitesting = new APITesting();
-//        String responseBody = apitesting.airBNBLogin(TestData.Login.EMAIL_SUSPENDED_LOGIN_PARAMS);
-//        System.out.println("Validate response for suspended login to AirBNB API using suspended email and password");
-//        Assert.assertEquals("Expected and Actual Response does not match",responseBody, TestData.TestLoginExpectedResults.SUSPENDED_ACCOUNT_RESPONSE);
-//    }
+    @Test
+    public void testFBLoginSuccessful() {
+        //cannot find valid fb auth code
+    }
+
+    @Test
+    public void testFBLoginInvalid() {
+        APITesting apitesting = new APITesting();
+        String responseBody = apitesting.airBNBLogin(TestData.Login.FB_INVALID_LOGIN_PARAMS);
+        System.out.println("Validate response for invalid login to AirBNB API using invalid fb authorization key");
+        Assert.assertEquals("Expected and Actual Response does not match",TestData.TestLoginExpectedResults.COULD_NOT_AUTHENTICATE_RESPONSE,responseBody);
+    }
+
+
+    @Test
+    public void testEmailLoginSuccessful() {
+        //cannot find valid email and password
+    }
+
+    @Test
+    public void testEmailLoginInvalidPassword() {
+        APITesting apitesting = new APITesting();
+        String responseBody = apitesting.airBNBLogin(TestData.Login.EMAIL_INVALID_PASSWORD_LOGIN_PARAMS);
+        System.out.println("Validate response for  invalid login to AirBNB API using  invalid email and valid password");
+        Assert.assertEquals("Expected and Actual Response does not match",TestData.TestLoginExpectedResults.INVALID_CREDENTIALS_RESPONSE,responseBody);
+    }
+
+    @Test
+    public void testEmailLoginInvalidEmail() {
+        APITesting apitesting = new APITesting();
+        String responseBody = apitesting.airBNBLogin(TestData.Login.EMAIL_INVALID_EMAIL_LOGIN_PARAMS);
+        System.out.println("Validate response for  invalid login to AirBNB API using valid email and invalid password");
+        Assert.assertEquals("Expected and Actual Response does not match",TestData.TestLoginExpectedResults.INVALID_CREDENTIALS_RESPONSE,responseBody);
+    }
+
+    @Test
+    public void testSuspendedEmailLoginInvalid() {
+        APITesting apitesting = new APITesting();
+        String responseBody = apitesting.airBNBLogin(TestData.Login.EMAIL_SUSPENDED_LOGIN_PARAMS);
+        System.out.println("Validate response for suspended login to AirBNB API using suspended email and password");
+        Assert.assertEquals("Expected and Actual Response does not match",TestData.TestLoginExpectedResults.SUSPENDED_ACCOUNT_RESPONSE,responseBody);
+    }
 
 
 }
